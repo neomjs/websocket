28f5b0029e4845b77419b38e8825c7758e9204c1
tobiu <tobiasuhlig78@gmail.com>
2022-04-25 11:56:34 +0200

    MyApp.view.MainContainer: added the UserTableContainer

--- apps/myapp/view/MainContainer.mjs
+++ apps/myapp/view/MainContainer.mjs
@@ -1,6 +1,7 @@
 import Component               from '../../../node_modules/neo.mjs/src/component/Base.mjs';
 import MainContainerController from './MainContainerController.mjs';
 import TabContainer            from '../../../node_modules/neo.mjs/src/tab/Container.mjs';
+import UserTableContainer      from './UserTableContainer.mjs';
 import Viewport                from '../../../node_modules/neo.mjs/src/container/Viewport.mjs';
 
 /**
@@ -12,33 +13,11 @@ class MainContainer extends Viewport {
         className : 'MyApp.view.MainContainer',
         autoMount : true,
         controller: MainContainerController,
-        layout    : {ntype: 'vbox'},
+        layout    : {ntype: 'vbox', align: 'stretch'},
 
         items: [{
-            module: TabContainer,
-            height: 300,
-            width : 500,
-            style : {flex: 'none', margin: '20px'},
-
-            itemDefaults: {
-                module: Component,
-                cls   : ['neo-examples-tab-component'],
-                style : {padding: '20px'},
-            },
-
-            items: [{
-                tabButtonConfig: {
-                    iconCls: 'fa fa-home',
-                    text   : 'Tab 1'
-                },
-                vdom: {innerHTML: 'Welcome to your new Neo App.'}
-            }, {
-                tabButtonConfig: {
-                    iconCls: 'fa fa-play-circle',
-                    text   : 'Tab 2'
-                },
-                vdom: {innerHTML: 'Have fun creating something awesome!'}
-            }]
+            module: UserTableContainer,
+            style : {flex: 'none', margin: '20px'}
         }, {
             ntype: 'toolbar',
             style: {margin: '20px'},
