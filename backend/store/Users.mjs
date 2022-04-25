75553475cf21493403a09d418e256927814ab06a
tobiu <tobiasuhlig78@gmail.com>
2022-04-25 16:23:50 +0200

    MyApp.backend.store.Users

---
+++ backend/store/Users.mjs
@@ -0,0 +1,35 @@
+import UserModel from '../../apps/myapp/model/User.mjs';
+import Store     from 'neo.mjs/src/data/Store.mjs';
+
+/**
+ * @class MyApp.backend.store.Users
+ * @extends Neo.data.Store
+ */
+class Users extends Store {
+    static getConfig() {return {
+        /**
+         * @member {String} className='MyApp.backend.store.Users'
+         * @protected
+         */
+        className: 'MyApp.backend.store.Users',
+        /**
+         * @member {Boolean} autoLoad=true
+         */
+        //autoLoad: true,
+        /**
+         * @member {Neo.data.Model} model=UserModel
+         */
+        model: UserModel,
+        /**
+         * @member {Object[]} sorters
+         */
+        sorters: [{
+            direction: 'ASC',
+            property : 'firstname'
+        }]
+    }}
+}
+
+Neo.applyClassConfig(Users);
+
+export default Users;
